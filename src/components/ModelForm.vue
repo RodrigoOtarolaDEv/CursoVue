989554da17ed335a29f94d07a6678138992db493
RodrigoOtarolaDEv <r.otarola.bass@gmail.com>
2024-06-11 10:43:46 -0400

    formulario checkbox y select

--- src/components/ModelForm.vue
+++ src/components/ModelForm.vue
@@ -1,6 +1,12 @@
 <template>
 <form>
   <input type="text" v-model="inputText"/>
+  <input type="checkbox" v-model="agree"/> Acepto los terminos y condiciones
+  <select v-model="countries">
+    <option value="1">Spain</option>
+    <option value="2">UK</option>
+    <option value="3">France</option>
+  </select>
 </form>
   <h3>{{inputText}}</h3>
 </template>
@@ -8,6 +14,10 @@
 <script lang="ts" setup>
 import {ref} from "vue";
 let inputText = ref('')
+// Para activar checkbox
+let agree = ref(true)
+// Seleccionar select
+let countries = ref(1)
 
 </script>
 
