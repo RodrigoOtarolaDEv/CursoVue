970cb30c888404ff2c0d0d87b4f6797ac52b486a
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2024-06-11 08:46:03 -0400

    Directiva selectivas(v-if y v-show) y de estructuras(v-for)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <CounterOption />
-  <CounterComposition />
+<!--  <CounterOption />-->
+<!--  <CounterComposition />-->
 <!--  <h1 >Listado de Post</h1>-->
 <!--  <PostContainer />-->
+
+<!--  <ExampleSelective />-->
+
+  <ExampleFor />
 
 </template>
 
 <script lang="ts" setup>
 
 // import PostContainer from "@/components/PostContainer.vue";
-import CounterOption from "@/components/CounterOption.vue";
-import CounterComposition from "@/components/CounterComposition.vue";
+// import CounterOption from "@/components/CounterOption.vue";
+// import CounterComposition from "@/components/CounterComposition.vue";
+// import ExampleSelective from "@/components/ExampleSelective.vue";
+import ExampleFor from "@/components/ExampleFor.vue";
 </script>
 
 <style>
---
+++ src/components/ExampleFor.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul>
+    <!--  :key es el identificador para cada elemento-->
+    <li v-for="country in countries" :key="country.code">{{ country.name }}</li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+import {Ref, ref} from "vue";
+interface ICountry{
+  code:string,
+  name: string
+}
+let countries:Ref<Array<ICountry>> = ref([
+  {
+    code: 'es',
+    name: 'Spain'
+  },
+  {
+    code: 'en',
+    name: 'UK'
+  }, {
+    code: 'fr',
+    name: 'France'
+  }, {
+    code: 'it',
+    name: 'Italy'
+  }, {
+    code: 'por',
+    name: 'Portugal'
+  }
+])
+</script>
+
+<style scoped>
+
+</style>
