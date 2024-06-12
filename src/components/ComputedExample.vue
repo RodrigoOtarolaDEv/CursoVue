7ae951edf9994116a1605280c67bea43dc205e7c
RodrigoOtarolaDEv <r.otarola.bass@gmail.com>
2024-06-12 08:26:36 -0400

    computed con composition api

--- src/components/ComputedExample.vue
+++ src/components/ComputedExample.vue
@@ -1,27 +1,21 @@
 <template>
   <div>
-    <input type="text" v-model="message" />
+    <input type="text" v-model="message"/>
     <h3>{{ message }}</h3>
     <h3>Valor computado: {{ reverse }}</h3>
   </div>
 </template>
 
-<script lang="js">
-import { defineComponent } from 'vue';
-export default defineComponent({
-  name: 'ComputedExample',
-  data(){
-    return{
-      message : ''
-    }
-  },
-  // Definimos valores computados
-  computed:{
-    reverse(){
-      return this.message.split('').reverse().join('')
-    }
-  }
-})
+<script lang="js" setup>
+
+import {computed, ref} from "vue";
+
+const message = ref('');
+
+// Definimos valores computados
+const reverse = computed(() =>
+  message.value.split('').reverse().join(''))
+
 </script>
 
 <style></style>
